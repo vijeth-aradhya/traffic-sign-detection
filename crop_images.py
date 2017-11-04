38909adb3be96c21a715b66dfa9117d9d93e51ce
vijeth-aradhya <vijthaaa@gmail.com>
2017-11-04 19:29:31 +0530

    Switch to OpenCV
    both crop and resize coded; dimension not decided.

--- crop_images.py
+++ crop_images.py
@@ -12,7 +12,7 @@ import os
 import sys
 
 import pandas as pd
-from PIL import Image
+import cv2
 
 train_dir = sys.argv[1]
 
@@ -28,6 +28,17 @@ for folder in folder_list:
         image_row = row[0].split(';')
         image = image_row[0]
         root = image[:-4]
-        im = Image.open(train_dir + folder + '/' + image)
-        im = im.crop((int(image_row[3]), int(image_row[4]), int(image_row[5]), int(image_row[6])))
-        im.save(train_dir + folder + '/' + root + '.jpg')
+        im = cv2.imread(train_dir + folder + '/' + image)
+        # cv2.imshow("original", im)
+        # cv2.waitKey(0)
+        # im = im[y1:y2, x1:x2] ; (x1,y1) as the top-left vertex and (x2,y2) as the bottom-right 
+        im = im[int(image_row[4]):int(image_row[6]), int(image_row[3]):int(image_row[5])]
+        # im.save(train_dir + folder + '/' + root + '.jpg')
+        """
+        To shrink an image, it will generally look best with CV_INTER_AREA interpolation,
+        whereas to enlarge an image, it will generally look best with CV_INTER_CUBIC (slow)
+        or CV_INTER_LINEAR (faster but still looks OK).
+
+        resized_img = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
+        resized_img = cv2.resize(im, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+        """
