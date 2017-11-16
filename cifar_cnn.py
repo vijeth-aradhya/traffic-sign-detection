915cf7327ed27a366a69e1968929423d97b05181
vijeth-aradhya <vijthaaa@gmail.com>
2017-11-16 21:10:49 +0530

    Minor changes to Net, printing and score

--- cifar_cnn.py
+++ cifar_cnn.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import keras
-from keras.datasets import cifar10
+from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -10,20 +10,19 @@ import os
 
 batch_size = 32
 num_classes = 43
-epochs = 50
+epochs = 100
 data_augmentation = True
-train_dir = "fill"
-validation_dir = "fill"
-test_dir= "fill"
-num_train_images = "fill"
-num_validation_images = "fill"
+train_dir = './GTSRB/Training/'
+validation_dir = "./GTSRB/Validation/"
+test_dir = "fill"
+num_train_images = 26145
+num_validation_images = 8053
 num_test_images = "fill"
-resize_dim = "fill"
+resize_dim = (100, 109)
 
 # Create CNN
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=x_train.shape[1:]))
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=(100, 109, 3)))
 model.add(Activation('relu'))
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
@@ -87,7 +86,7 @@ else:
         target_size=(resize_dim[0], resize_dim[1]), # Dimensions to which all the images will be resized
         batch_size=batch_size, # Default value
         class_mode='categorical', # Not sure about this
-        shuffle=False, # Might help (causes problems if True,sometimes)
+        shuffle=False, # Might help (causes problems if True, sometimes)
         save_to_dir=None) # Saving to visualize the augmented images
 
     validation_generator = datagen.flow_from_directory(
@@ -98,6 +97,7 @@ else:
         shuffle=False,
         save_to_dir=None)
 
+    """
     test_generator = datagen.flow_from_directory(
         test_dir,
         target_size=(resize_dim[0], resize_dim[1]),
@@ -105,31 +105,33 @@ else:
         class_mode="categorical", # Not sure about this
         shuffle=False,
         save_to_dir=None)
+    """
 
     print('Printing the train_data files')
-    print train_generator.filenames
+    print(train_generator.filenames)
 
     # Take images from dir in batches
     model.fit_generator(train_generator,
-                        steps_per_epoch=(num_train_images/batch_size),
+                        steps_per_epoch=int(num_train_images/batch_size),
                         epochs=epochs,
                         workers=4,
                         use_multiprocessing=False,
                         validation_data=validation_generator,
-                        validation_steps=(num_validation_images/batch_size))
+                        validation_steps=int(num_validation_images/batch_size))
 
 # Note we can do the score part of ourselves (predicing labels by taking img one by one
 
 # Score trained model on validation data
-validation_scores = model.evaluate_generator(self,
+validation_scores = model.evaluate_generator(
                                   validation_generator,
-                                  (num_validation_images/batch_size),
+                                  steps=int(num_validation_images/batch_size),
                                   workers=4,
                                   use_multiprocessing=False)
 
 # Index is 0 because we have provided only one metric in model?! Not sure!
-print('Validation Accuracy:', validation_scores[0])
+print('Validation Output:', validation_scores)
 
+"""
 # Score trained model on test data
 test_scores = model.evaluate_generator(self,
                                   test_generator,
@@ -139,3 +141,4 @@ test_scores = model.evaluate_generator(self,
 
 # Index is 0 because we have provided only one metric in model?! Not sure!
 print('Test Accuracy:', test_scores[0])
+"""
